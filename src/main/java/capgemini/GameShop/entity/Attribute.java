71d0b9da6711c315d9cd9a26913f9be858effa83
michaltworuszka <michal.tworuszka@gmail.com>
2022-06-08 14:46:22 +0200

    adress

--- src/main/java/capgemini/GameShop/entity/Attribute.java
+++ src/main/java/capgemini/GameShop/entity/Attribute.java
@@ -5,4 +5,24 @@ public class Attribute {
     private Long id;
     private String name;
 
+    public Attribute(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
